974a6208a1295df257bbcddb3c6d930dc0a816ce
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-09-02 10:26:45 +0200

    push to gh

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "marie", last_name: "langodewnjf")
+    assert_equal "Marie Langodewnjf", user.full_name
+  end
 end
