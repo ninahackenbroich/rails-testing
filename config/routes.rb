f99e4777d78ed0c1f63a119c384b375fbd40eebe
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2022-09-02 09:23:39 +0200

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  # get 'products/index'
   devise_for :users
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
 end
